a7b2480a972eef1e2787874fe934ef929fbc9c46
Akley Alves <akleyalvescr@gmail.com>
2023-02-07 23:00:04 -0300

    att

---
+++ TestePontoFlutuante.java
@@ -0,0 +1,11 @@
+public class TestePontoFlutuante {
+    public static void main(String[] args) {
+        double salario;
+        salario = 1250.70;
+        System.out.println("Meu salário é " + salario + "!");
+
+        	
+		double idade = 45; //Outro modo de usar váriavel
+		System.out.print(idade);
+    } 
+}
