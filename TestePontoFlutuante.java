754cfc6453611072f3bf847b7384e40583d23043
Akley Alves <akleyalvescr@gmail.com>
2023-02-07 23:13:14 -0300

    Update TestePontoFlutuante.java

--- TestePontoFlutuante.java
+++ TestePontoFlutuante.java
@@ -7,5 +7,17 @@ public class TestePontoFlutuante {
         	
 		double idade = 45; //Outro modo de usar váriavel
 		System.out.print(idade);
+
+
+        double divisao = 3.14 / 2;
+		System.out.print(divisao);
+
+        	
+		int outraDivisao = 5 / 2; //O resultado será 2. Java tem suas particularidades. 
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0 / 2; // Irá dar certo, pois o java lê a partir da direita.
+		System.out.println(novaTentativa);
+		
     } 
 }
