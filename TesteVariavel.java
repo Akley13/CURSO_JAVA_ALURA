1c00293256b0c9ec9d58068c610037123cf4c582
Akley Alves <akleyalvescr@gmail.com>
2023-02-07 22:34:05 -0300

    att

--- TesteVariavel.java
+++ TesteVariavel.java
@@ -1,5 +1,18 @@
 public class TesteVariavel {
     public static void main(String[] args) {
-        System.out.print("Olá teste Novo!!!");
+        System.out.println("Qual sua idade ?");
+
+        int idade;
+        idade = 21;
+
+        System.out.print(idade);
+
+        idade = 30 + 20;
+		
+		System.out.println(idade);
+		
+		idade = (2 + 2) * 5;
+		
+		System.out.print(idade);
     }
 }
