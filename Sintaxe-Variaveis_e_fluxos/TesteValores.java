c0779d306e843b549854f41fa24773cc6e2658e8
Akley Alves <akleyalvescr@gmail.com>
2023-02-08 23:39:21 -0300

    att

--- Sintaxe-Variaveis_e_fluxos/TesteValores.java
+++ Sintaxe-Variaveis_e_fluxos/TesteValores.java
@@ -1,3 +1,12 @@
 public class TesteValores {
-    
+    public static void main(String[] args) {
+        int primeiro = 5;
+		int segundo = 10;
+		segundo = primeiro;
+		primeiro = 10;
+		
+		// quanto vale o segundo ?
+		
+		System.out.print(segundo); // vale 5
+    }
 }
